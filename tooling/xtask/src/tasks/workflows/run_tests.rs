754efd45d8674d29a71444e1e815d2ef40f582f8
MrSubidubi <finn@zed.dev>
2026-02-26 00:49:05 +0100

    Fixes

--- tooling/xtask/src/tasks/workflows/run_tests.rs
+++ tooling/xtask/src/tasks/workflows/run_tests.rs
@@ -316,7 +316,7 @@ fn tests() -> Step<Use> {
     named::uses("namespacelabs", "breakpoint-action", "v0")
         .if_condition(Expression::new("failure()"))
         .add_with(("duration", "15m"))
-        .add_with(("user", "MrSubidubi"))
+        .add_with(("authorized-users", "MrSubidubi"))
 }
 
 fn check_dependencies() -> NamedJob {
